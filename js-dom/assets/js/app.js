6b77b55c87cae0a1ad0363e72b24c7e4d59635a2
FabioReact <fabio.react.dev@gmail.com>
2022-06-14 17:41:10 +0200

    feat: add class Product to js-dom exercice

--- js-dom/assets/js/app.js
+++ js-dom/assets/js/app.js
@@ -3,22 +3,58 @@ console.log("Script App launched");
 const app = document.getElementById("app");
 app.innerHTML = "Content injected by Javascript";
 
+class Product {
+	constructor({ id, name, description, price, image }) {
+		this.id = id
+		this.name = name
+		this.description = description
+		this.price = price
+		this.image = image
+	}
+
+	render() {
+		const article = document.createElement("article");
+		article.classList.add('rounded-xl', 'flex', 'flex-col', 'w-72', 'shadow-lg', 'overflow-hidden')
+		article.innerHTML = `
+			<img src="${this.image}" alt="${this.name}" />
+			<div class="p-4">
+				<h3 class="uppercase tracking-widest text-center text-xl font-light">${this.name}</h3>
+				<p class="text-gray-600 text-sm py-2">${this.description}</p>
+				<div class="flex">
+					<span class="text-blue-700 font-semibold">${this.price}€</span>
+					<button class="border border-gray-400 text-gray-800 rounded text-sm px-2 ml-auto">Add to cart</button>
+				</div>
+			</div>
+		`;
+		return article
+	}
+}
+
+class App {
+	static init() {
+		for (const product of products) {
+			const productItem = new Product(product)
+			const article = productItem.render()
+			app.appendChild(article)
+		}
+	}
+}
+
 const products = [
 	{
 		id: 1,
-		name: 'Sweater',
-		description: 'Christmas sweater',
+		name: "Sweater",
+		description: "Christmas sweater",
 		price: 49,
-		image: 'https://m.media-amazon.com/images/I/91zgO6wYIfL._AC_UY445_.jpg'
+		image: "https://m.media-amazon.com/images/I/91zgO6wYIfL._AC_UY445_.jpg",
 	},
 	{
 		id: 1,
-		name: 'Tshirt',
-		description: 'Lorem ipsum dolor sit amet.',
-		price: 49,
-		image: 'https://m.media-amazon.com/images/I/91zgO6wYIfL._AC_UY445_.jpg'
+		name: "Tshirt",
+		description: "Colorful Shirt",
+		price: 20,
+		image: "https://cdn.hoodielab.com/wp-content/uploads/2020/12/4_T-Shirt-Full-Edit-Front-6-400x400.jpg",
 	},
-]
+];
 
-// Exercice 1
-// Pour chaque produit, créer une vignette dans une liste et l'afficher
+App.init();

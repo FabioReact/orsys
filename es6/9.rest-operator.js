8f8bfdc0047e993aeb01545af2db7693be6ea1ee
FabioReact <fabio.react.dev@gmail.com>
2022-06-14 13:43:32 +0200

    feat: add rest operator reminder

---
+++ es6/9.rest-operator.js
@@ -0,0 +1,8 @@
+// Rest operator - semblable au spread operator dans son écriture, son utilisation est cependant différente
+const sum = (...numbers) => {
+	return numbers.reduce((total, n) => total + n, 0)
+}
+
+console.log(sum(1)) // 1
+console.log(sum()) // 0
+console.log(sum(2, 3, 5)) // 10

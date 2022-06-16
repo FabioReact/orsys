fe95b5049732dc38727bf70e2f5418ba16331828
FabioReact <fabio.react.dev@gmail.com>
2022-06-16 11:44:27 +0200

    feat: add useEffect on update

--- react-initiation/src/components/FetchPosts/FetchPosts.jsx
+++ react-initiation/src/components/FetchPosts/FetchPosts.jsx
@@ -13,13 +13,25 @@ const FetchPosts = () => {
 	const [posts, setPosts] = useState([]);
 	useEffect(() => {
 		console.log("Ne va s'executer que lors de la création du composant");
-		fetch("https://jsonplaceholder.typicode.com/posts")
+		const controller = new AbortController();
+		fetch("https://jsonplaceholder.typicode.com/posts", {
+			method: "GET",
+			signal: controller.signal,
+		})
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
 				setPosts(data);
 			});
+		return () => {
+			console.log("Destruction du composant");
+			// Ici, j'annule le fetch si celui-ci ne s'est pas terminé et que le composant va se démonter de l'UI
+			controller.abort();
+		};
 	}, []);
+	useEffect(() => {
+		console.log("Création et mise à jour de posts");
+	}, [posts]);
 	return (
 		<section>
 			<h2>JSON Placeholder Posts</h2>
